4d5c5d005048d3335e860d805d4e631abf6d857a
vovantrinhdhbkdn@gmail.com <Trinh Vo>
2025-10-22 20:34:17 +0700

    init source code back-end & front-end with integration auth0 for authentication

---
+++ front-end/src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      Home
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ front-end/src/app/admin/layout.tsx
@@ -0,0 +1,24 @@
+import AppSidebar from "@/components/AppSidebar";
+import Navbar from "@/components/Navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
+  
+  return (
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar />
+        <main className="w-full">
+          <Navbar />
+          <div className="px-4">{children}</div>
+        </main>
+      </SidebarProvider>
+  );
+}
